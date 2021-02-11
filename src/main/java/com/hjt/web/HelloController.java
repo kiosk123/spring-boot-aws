342c8196d035ad366dd5e74da8210347a51c38ae
Heo Jong Tae <heo9910@naver.com>
2021-02-11 23:45:19 +0900

    .

--- src/main/java/com/hjt/web/HelloController.java
+++ src/main/java/com/hjt/web/HelloController.java
@@ -1,5 +1,7 @@
 package com.hjt.web;
 
+import com.hjt.web.dto.HelloResponseDto;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +11,10 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(String name, int amount) {
+        return new HelloResponseDto(name, amount);
     }
 }
--- src/test/java/com/hjt/web/HelloControllerTest.java
+++ src/test/java/com/hjt/web/HelloControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -22,5 +23,19 @@ public class HelloControllerTest {
         mvc.perform(get("/hello"))
             .andExpect(status().isOk())
             .andExpect(content().string(hello));
+    }
+
+    @Test
+    public void helloResponseDtoReturn() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(get("/hello/dto")
+                    .param("name", name)
+                    .param("amount", String.valueOf(amount))
+                    .contentType(MediaType.APPLICATION_JSON))
+           .andExpect(status().isOk())
+           .andExpect(jsonPath("name").value(name))
+           .andExpect(jsonPath("amount").value(amount));
     }
 }
